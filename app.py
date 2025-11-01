81377212879030c66074ea490cee59e5c6da1888
00067922-spec <00067922@uca.edu.sv>
2025-11-01 15:52:54 -0600

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@
 import streamlit as st
 from modulos.venta import mostrar_venta  # Importamos la función mostrar_venta del módulo venta
 from modulos.login import login  # Importamos la función login
-
 # Comprobamos si la sesión ya está iniciada
 if "sesion_iniciada" in st.session_state and st.session_state["sesion_iniciada"]:
     # Si la sesión está iniciada, mostrar el contenido de ventas
