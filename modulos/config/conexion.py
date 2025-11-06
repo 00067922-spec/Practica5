8441718dd2105be01a289c533bcd95fb8f8b5e80
00067922-spec <00067922@uca.edu.sv>
2025-11-06 06:17:56 -0600

    Update conexion.py

--- modulos/config/conexion.py
+++ modulos/config/conexion.py
@@ -4,10 +4,10 @@ from mysql.connector import Error
 def obtener_conexion():
     try:
         conexion = mysql.connector.connect(
-            host='bajbkalh7yvvxmv9sb7k-mysql.services.clever-cloud.com',
-            user='usgfd3oz3rlvjd4k',
-            password='35K9q9MbM8PKSDCGs9Zh',
-            database='bajbkalh7yvvxmv9sb7k',
+            host='be5bmntqvmjb45dbc68h-mysql.services.clever-cloud.com',
+            user='ufrsewvahgrdaghy',
+            password='UxDnJbPxibZaLwBC6Xt1',
+            database='be5bmntqvmjb45dbc68h',
             port=3306
         )
         if conexion.is_connected():
@@ -19,3 +19,5 @@ def obtener_conexion():
     except mysql.connector.Error as e:
         print(f"❌ Error al conectar: {e}")
         return None
+
+
